32eec45755b396a8dded9fc60bc41fef6b82fef1
eugenelyy25 <looyungyi@gmail.com>
2024-09-16 14:57:08 +0100

    160924_c3

--- E_E/1.17.1_primes.c
+++ E_E/1.17.1_primes.c
@@ -1,14 +1,35 @@
 #include <stdio.h>
 
-int prime(int n);
+int is_prime(int n);
 
-int main(void)
-{
-    int n;
-    printf("%i\n")
+int main(void){
+    int num;
 
-    return 0
+    do {
+        printf("Enter a number: ");
+        num = scanf ("%d", &num);
+    }
+
+    while (is_prime (1));//check input
+        printf("%d\n", num);
+        num = is_prime(num);
+
+    if (is_prime (0)){
+        printf("%d is not a prime number.", num);
+        }
+    return 0;
 }
 
-int prime(int n);
-if  (n <= 1);
+int is_prime(int n){
+    int count = 0;
+    for (int i = 1; i < n; i++){ //initialize input number
+        if (n%i == 0){ //to check when number input divided has remainder
+        count++;} //add the counter
+    }
+        if (count == 2){
+            return 1; //when counter returns 2 is prime
+        }
+        else{
+            return 0; //when counter returns more than 2 is not prime
+        }
+}
