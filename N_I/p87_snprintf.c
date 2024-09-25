3b1d85eecb7f15df90ae7a7bb19ea875a47d8a3f
eugenelyy25 <looyungyi@gmail.com>
2024-09-25 17:57:28 +0100

    250924_c3

--- N_I/p87_snprintf.c
+++ N_I/p87_snprintf.c
@@ -7,5 +7,16 @@ void test(void)
     init_deck(d);
     // Direct Assignment
     print_card(str, d[0]);
-    // 
+    // 1st element initialised correctly
+    assert(strcmp(str, FIRSTCARD)==0);
+    for(int i=0; i<1000; i++){
+        shuffle_deck(d);
+        print_card(str, d[0]);
+        // Happens 1 time in 52?
+        if(strcmp(str, FIRSTCARD)==0){
+            n++;
+        }
+    }
+    // Is this a reasonable test ?
+    assert((n > 10) && (n < 30));
 }
